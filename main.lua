fd0c33cffa0a1acf05d93b9ad8bf7e2f207773b9
Kristofer Lindberg <k.lindberg@gmail.com>
2013-12-14 21:09:02 +0100

    their number is increasing

--- main.lua
+++ main.lua
@@ -1,6 +1,10 @@
-debug = true
+debug = false
+paused = false
 WIDTH = 100
 HEIGHT = 50
+PLANT_ENERGY = 100
+REPRODUCTION_ENERGY = 200
+GENES = 8
 
 function random_pos(w, h)
    local width = math.random(w)
@@ -10,7 +14,7 @@ end
 
 function random_genes()
    genes = {}
-   for i = 1,8 do
+   for i = 1,GENES do
       genes[#genes+1] = math.random(10)
    end
    return genes
@@ -22,14 +26,16 @@ function get_coord(c, r)
    return x, y
 end
 
-function make_animal()
+function make_animal(species, energy)
    local animal = {}
    local c,r = random_pos(WIDTH,HEIGHT)
    animal["c"] = c
    animal["r"] = r
-   animal["energy"] = 1000
+   animal["energy"] = energy
    animal["dir"] = 0
-   animal["genes"] = random_genes
+   animal["genes"] = random_genes()
+   animal["species"] = species
+   animal["color"] = {math.random(255), math.random(255), math.random(255)}
    return animal
 end
 
@@ -49,6 +55,7 @@ function move_animal(animal)
       animal.r = animal.r + 1
    end
    animal.r = (animal.r + HEIGHT) % HEIGHT
+   animal.energy = animal.energy - 1
    if debug then print("after:", animal.c, animal.r) end
 end
 
@@ -61,22 +68,68 @@ function sum_genes(t)
 end
 
 function turn_animal(animal)
-   print(sum_genes(animal.genes))
-end
-
-function print_table(t)
-   for _,v in ipairs(t) do
-      print(v)
+   sum_genes(animal.genes)
+   r = math.random(sum)
+   for i,v in ipairs(animal.genes) do
+      r = r - v
+      if r < 0 then
+	 animal.dir = (animal.dir + i) % 8
+	 if debug then print("we are turning, we are turning", animal.dir, "is the new new") end
+	 break
+      end
    end
 end
 
+function eat_animal(animal)
+   local index = animal.c..":"..animal.r
+   if plants[index] ~= nil then
+      if debug then print("Woho, we have found food at", index) end
+      animal.energy = animal.energy + PLANT_ENERGY
+      plants[index] = nil
+      if debug then print("New energy level is", animal.energy) end
+   end
+end
+
+function mutate_animal(animal)
+   local gene = math.random(1,GENES)
+   animal.genes[gene] = animal.genes[gene] + math.random(-1,1)
+end
+
+function reproduce_animal(animal)
+   if animal.energy > REPRODUCTION_ENERGY then
+      if debug then print("Getting a child") end
+      animal.energy = animal.energy / 2
+      if debug then print("Energy", animal.energy) end
+      child = make_animal("child", animal.energy)
+      if debug then print_table(child.genes) end
+      mutate_animal(child)
+      if debug then print_table(child.genes) end
+      animals[#animals+1] = child
+   end
+end
+
+function add_plants()
+   for i = 1,2 do
+      c, r = random_pos(WIDTH, HEIGHT)
+      plants[c..":"..r] = {c = c, r = r}
+   end
+end
+
+function print_table(t)
+   values = "["
+   for _,v in ipairs(t) do
+      values = values..v..", "
+   end
+   print(values.."]")
+end
+
 function love.load()
-   game_clock = 0
+   generation = 0
    ground = {189, 183, 107}
 
    plants = {}
    plant = {139, 0, 139}
-   for i = 1,10 do
+   for i = 1,50 do
       c, r = random_pos(WIDTH, HEIGHT)
       plants[c..":"..r] = {c = c, r = r}
    end
@@ -92,21 +145,34 @@ function love.load()
    animals = {}
    animal = {255, 20, 147}
    for i = 1,1 do
-      animals[#animals+1] = make_animal()
+      animals[#animals+1] = make_animal("parent", 1000)
    end
 end
 
 function love.keypressed(key, unicode)
+   if key == "`" then
+      debug = not debug
+   end
+   if key == " " then
+      paused = not paused
+   end
 end
 
 function love.update(dt)
-   game_clock = game_clock + dt
-   if game_clock > 0.5 then
-      for _,v in ipairs(animals) do
-	 move_animal(v)
+   if paused then return end
+   generation = generation + 1
+   if debug then print("Number of animals", #animals) end
+   for i,v in ipairs(animals) do
+      if v.energy <= 0 then
+	 table.remove(animals, i)
+	 if debug then print("Removing an animal, population down to", #animals) end
       end
-      game_clock = 0
+      move_animal(v)
+      turn_animal(v)
+      eat_animal(v)
+      reproduce_animal(v)
    end
+   add_plants()
 end
 
 function love.draw()
@@ -150,9 +216,13 @@ function love.draw()
    end
 
    -- draw the animals
-   love.graphics.setColor(animal)
+   --love.graphics.setColor(animal)
    for _,v in ipairs(animals) do
+      love.graphics.setColor(v.color)
       x, y = get_coord(v.c, v.r)
       love.graphics.rectangle("fill", x, y, 8, 10)
    end
+
+   love.graphics.setColor(0,0,0)
+   love.graphics.print("Generation: "..generation, 0, 100)
 end
