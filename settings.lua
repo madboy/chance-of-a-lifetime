93cec5f746bb909e3489edf199d4f3d24921b80f
Kristofer Lindberg <k.lindberg@gmail.com>
2013-12-16 21:42:55 +0100

    moving some config

---
+++ settings.lua
@@ -0,0 +1,17 @@
+settings = {}
+
+settings.debug = false
+settings.paused = true
+settings.game_over = false
+settings.game_started = false
+
+settings.width = 100
+settings.height = 50
+
+settings.plant_energy = 80
+settings.animal_energy = 100
+settings.reproduction_energy = 200
+
+settings.genes = 8
+
+return settings
